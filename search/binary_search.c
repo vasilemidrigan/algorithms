b6446d6819f32ef9cb8509aa7b97461f110fa4e5
Vasile Midrigan <111rigan111@gmail.com>
2023-06-30 13:18:13 +0100

    implement booleans instead of integer returns

--- search/binary_search.c
+++ search/binary_search.c
@@ -1,4 +1,12 @@
-// Binary Search can only be implemented into a sorted list of items. 
+// Binary Search can only be implemented into a sorted list of items, 
+// it works by setting a middle of lower and upper boundaries, 
+// if the element we are searching for is at the current index, 
+// then the value is found and the algorithm operation is done, 
+// else if the element is greather than the value at the current index  then 
+// we're setting lower bound to middle + 1,
+// else if the element is lower than the value at the current index, then 
+// we're setting the upper bound to the middle - 1,
+// and so on until the lower bound is greather
 
 /* x = 4
 	* lo = arr[0]
@@ -17,24 +25,27 @@
 	* */
 
 #include <stdio.h>
+#include <stdbool.h>
 
-int binary_search(int a[], int size, int n);
+bool binary_search(int a[], int size, int n);
 
 int main() {
 			int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		 int size = sizeof(arr) / sizeof(arr[0]);
 
-		 int is_value_found = binary_search(arr, size, 8);	
+		 bool is_value_found = binary_search(arr, size, 10);	
+			printf("%s", is_value_found ? "true": "false");
 }
 
-int binary_search(int a[], int size, int n) {
+bool binary_search(int a[], int size, int n) {
 				int lo = 0;
 				int hi = size - 1;
-	
+					
 				while(lo <= hi) {
+								printf("lo: %i, hi: %i\n", lo, hi);
 								int mid = lo + (hi - lo) / 2;
 								if(a[mid] == n) {
-												return mid; // Success	
+												return true; // Success	
 								}	
 								else if(a[mid] < n) {
 												lo = mid + 1;		
@@ -44,5 +55,5 @@ int binary_search(int a[], int size, int n) {
 								}
 				}		
 				
-				return 1; // Fail
+				return false;
 }
--- search/linear_search.c
+++ search/linear_search.c
@@ -5,26 +5,28 @@
 // until the desired element in meet.
 
 #include <stdio.h>
+#include <stdbool.h>
 
 int linear_search(int array[], int length, int value);
 
 int main(void) {
-				
+
 				int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 				int length = sizeof(arr) / sizeof(arr[0]);
-				
+
 				int is_value_found = linear_search(arr, length, 8);
+				printf("%s", is_value_found ? "true": "false");
 }
 
 int linear_search(int array[], int length, int value) {
-			 
-			 // iterate through each element of the array and compare it with the value
-				// we are looking for. If it is found return 0 (success) else return 1 (fail)	
+
+				// iterate through each element of the array and compare it with the value
+				// we are looking for. If it is found return true, otherwise false.
 				for(int i = 0; i < length; i++) {
 								if(array[i] == value) {
-												return 0; 
+												return true; 
 								}	
 				}
 
-				return 1; 
+				return false; 
 }
